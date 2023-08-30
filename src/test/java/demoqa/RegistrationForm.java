bab21b7e0a78c9365db98879594d26ccff49cec8
NovruzSalakhov <salakhov-novruz@mail.ru>
2023-08-30 19:44:14 +0300

    Added new class RegistrationForm which is true

--- src/test/java/demoqa/RegistrationForm.java
+++ src/test/java/demoqa/RegistrationForm.java
@@ -3,8 +3,6 @@ package demoqa;
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
 
 import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
@@ -13,8 +11,7 @@ import org.junit.jupiter.api.Test;
 
 
 
-public class RegistrationTest {
-
+public class RegistrationForm {
     @BeforeAll
     static void beforeAll() {
         Configuration.baseUrl = "https://demoqa.com";
@@ -24,16 +21,16 @@ public class RegistrationTest {
     }
 
     @Test
-    void successfulRegistrationTest () {
+    void fillFormTest() {
         open("/automation-practice-form");
         executeJavaScript("$('#fixedban').remove()");
         executeJavaScript("$('footer').remove()");
 
         $("#firstName").setValue("Novruz");
         $("#lastName").setValue("Salakhov");
-        $("#userEmail").setValue("Salakhov@mail.ru");
+        $("#userEmail").setValue("Novruz@mail.ru");
         $("#genterWrapper").$(byText("Male")).click();
-        $("#userNumber").setValue("89258832030");
+        $("#userNumber").setValue("8925884338");
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").$(byText("May")).click();
         $(".react-datepicker__year-select").$(byText("1993")).click();
@@ -49,10 +46,11 @@ public class RegistrationTest {
 
         $("#example-modal-sizes-title-lg").shouldHave(text("Thanks for submitting the form"));
 
+
         $x("//td[contains(text(), 'Student Name')]/../td[2]").shouldHave(text("Novruz Salakhov"));
-        $x("//td[contains(text(), 'Student Email')]/../td[2]").shouldHave(text("Salakhov@mail.ru"));
+        $x("//td[contains(text(), 'Student Email')]/../td[2]").shouldHave(text("Novruz@mail.ru"));
         $x("//td[contains(text(), 'Gender')]/../td[2]").shouldHave(text("Male"));
-        $x("//td[contains(text(), 'Mobile')]/../td[2]").shouldHave(text("89258832030"));
+        $x("//td[contains(text(), 'Mobile')]/../td[2]").shouldHave(text("8925884338"));
         $x("//td[contains(text(), 'Date of Birth')]/../td[2]").shouldHave(text("17 May,1993"));
         $x("//td[contains(text(), 'Subjects')]/../td[2]").shouldHave(text("English"));
         $x("//td[contains(text(), 'Hobbies')]/../td[2]").shouldHave(text("Sports"));
